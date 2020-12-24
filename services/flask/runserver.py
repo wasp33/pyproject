f956805737c1aa0faf138a3ab2b5a12e6044409b
Joseph Liu <drowsy.liu@gmail.com>
2020-12-24 14:46:07 +0800

    first runserver introduce

--- services/flask/runserver.py
+++ services/flask/runserver.py
@@ -2,27 +2,27 @@
 from flask import Flask,request
 from datetime import datetime
 import logging
-import flask_babel
+from flask_babel import *
 
 
 app = Flask(__name__)
 app.config.from_pyfile('settings.cfg')
-babel=Babel(app)
+babel = Babel(app)
 
-def error_log(logstr):
+def error_log(app,logstr):
     currtimestring = datetime.now().strftime('%Y%m%d %H:%M:%S')
     app.logger.info(currtimestring+' %s'%logstr)
 
 @app.route('/test')
 def get_test():
-    print('test')
+    error_log(app,'call test')
     return 'OK'
 
 if __name__ == '__main__':
     app.debug = True
-    log_filename = datetime.now().strftime("msg_%Y-%m-%d.log")
+    log_filename = datetime.now().strftime("log_%Y-%m-%d.log")
     handler = logging.FileHandler(log_filename)
     print("app--------------------")
     app.logger.addHandler(handler)
-    error_log('web start ')
+    error_log(app,'web start ')
     app.run(host='0.0.0.0',port='5000',debug=True)
